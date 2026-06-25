2a5789cc32fedf6aac74fe975b150cd481e04942
CaldoO-O <gabcaldo@gmail.com>
2026-06-25 14:19:11 -0300

    Agora é utf-8

--- message_router.py
+++ message_router.py
@@ -6,13 +6,13 @@ def encode_message(data : dict) -> bytes:
     Transforma um dicionário Python em uma string JSON codificada em bytes,
     adicionando uma quebra de linha ('\n') no final para servir de delimitador.
     """
-    return (json.dumps(data) + "\n").encode()
+    return (json.dumps(data) + "\n").encode('utf-8')
 
 def decode_message(raw : bytes) -> dict:
     """
     Decodifica uma sequência de bytes recebida da rede (que contém uma string JSON)
     e reconstrói o dicionário Python original.
     """
-    return json.loads(raw.decode())
+    return json.loads(raw.decode('utf-8'))
 
 
